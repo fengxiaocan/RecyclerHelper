6005ff4c91a1fe4c8292a3aa11a8b817d9db5410
fengyongcan <fengxiaocan@gmail.com>
2019-01-03 17:34:52 +0800

    1.添加findViewById方法

--- helper/src/main/java/com/evil/helper/recycler/holder/BaseRecyclerHolder.java
+++ helper/src/main/java/com/evil/helper/recycler/holder/BaseRecyclerHolder.java
@@ -1,6 +1,7 @@
 package com.evil.helper.recycler.holder;
 
 import android.content.Context;
+import android.support.annotation.IdRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -27,4 +28,12 @@ public abstract class BaseRecyclerHolder extends RecyclerView.ViewHolder {
     }
     
     public void onBindData(RecyclerView.Adapter adapter,int position){}
+    
+    public final <T extends View> T findViewById(@IdRes int id) {
+        if (id == View.NO_ID) {
+            return null;
+        }
+        return itemView.findViewById(id);
+    }
+
 }
